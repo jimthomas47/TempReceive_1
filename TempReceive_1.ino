842e60635e7b6712443f23d20e846021250b96d6
Jim Thomas <jthomas@sasaustin.org>
2015-07-28 11:25:47 -0500

    Modify serial print for node.js
    Change the serial prints to print Outside, Attic, Office temperature as
    3 space separated numbers.  First attempt to interface to node.js and
    use plotly

--- TempReceive_1.ino
+++ TempReceive_1.ino
@@ -1,6 +1,8 @@
 /*
   Modified version of LOG's Temperature monito program
   Jim Thomas
+
+  7/28/15 changed serial print to compatible with node.js / plotly v1
 */
 
 /*********************************************************************
@@ -36,11 +38,11 @@ All text above, and the splash screen must be included in any redistribution
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// DS18B20 is plugged into Arduino D4 
+// DS18B20 is plugged into Arduino D4
 #define ONE_WIRE_BUS 4
 // Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(ONE_WIRE_BUS);
-// Pass our oneWire reference to Dallas Temperature. 
+// Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 // This is DS18B20 #20 address
 DeviceAddress Therm1 = { 0x28, 0x44, 0x15, 0x5D, 0x05, 0x00, 0x00, 0x69 };  //unit #3
@@ -55,8 +57,8 @@ unsigned long clearTime = 240000;	// in milliseconds
 
 // Number of Temperature sensors
 #define NumNodes 3
-// nRF24L01(+) radio attached 
-RF24 radio(9,10);
+// nRF24L01(+) radio attached
+RF24 radio(9, 10);
 // Network uses that radio
 RF24Network network(radio);
 
@@ -76,10 +78,10 @@ struct payload_t
 };
 
 // Since nodes start at 1, I added 1 to arrays to make it easier
-unsigned long NodeCounter[NumNodes+1];
-float NodeTempC[NumNodes+1];
-float NodeTempF[NumNodes+1];
-float NodeVcc[NumNodes+1];
+unsigned long NodeCounter[NumNodes + 1];
+float NodeTempC[NumNodes + 1];
+float NodeTempF[NumNodes + 1];
+float NodeVcc[NumNodes + 1];
 
 // LCD5110 connections
 // pin 3 - Serial clock out (SCLK)
@@ -96,47 +98,48 @@ void displayData();					// display data
 void getRadioData();				// get Radio data
 void clearTemperatures();			// clear temps to make sure modules are active
 
-void setup(){
-	// Start up the library
-	sensors.begin();
-    // set the Temp resolution to 12 bit 
-	sensors.setResolution(Therm1, 12);
+void setup() {
+  // Start up the library
+  sensors.begin();
+  // set the Temp resolution to 12 bit
+  sensors.setResolution(Therm1, 12);
 
-	Serial.begin(9600);
-	Serial.println("Temperature Data");
- 
-	SPI.begin();
-	// Radio setup
-	radio.begin();
-	// network.begin(/*channel*/, /*node address*/);
-	network.begin(channel, this_node);
-	
-	display.begin();
-	display.setContrast(67);	// Choose best contrast - default 55
-	display.display(); 			// show splashscreen
-	delay(20);
-	display.clearDisplay();   	// clears the screen and buffer
-	
-	startTime = millis();		// start the timer
+  Serial.begin(9600);
+//  Serial.println("Temperature Data");
+
+  SPI.begin();
+  // Radio setup
+  radio.begin();
+  // network.begin(/*channel*/, /*node address*/);
+  network.begin(channel, this_node);
+
+  display.begin();
+  display.setContrast(67);	// Choose best contrast - default 55
+  display.display(); 			// show splashscreen
+  delay(20);
+  display.clearDisplay();   	// clears the screen and buffer
+
+  startTime = millis();		// start the timer
 
 }
 
 void loop() {
-	// Pump the network regularly
-	network.update();
-	getTemperature(Therm1);	
-	// Is there anything ready for us?
-	while ( network.available() ){
-		// If so, grab it and print it out
-		getRadioData();
-	}
+  // Pump the network regularly
+  network.update();
+  getTemperature(Therm1);
+  // Is there anything ready for us?
+  while ( network.available() ) {
+    // If so, grab it and print it out
+    getRadioData();
+  }
 
-	displayData();
-	delay(100);
-	if ( (millis() - startTime) > clearTime){ 
-		clearTemperatures();  
-		startTime = millis();		// restart the timer
-	}
+
+  displayData();
+  delay(100);
+  if ( (millis() - startTime) > clearTime) {
+    clearTemperatures();
+    startTime = millis();		// restart the timer
+  }
 }
 
 //////////////////////////////////////////////////////////////////
@@ -144,91 +147,106 @@ void loop() {
 //////////////////////////////////////////////////////////////////
 void getTemperature(DeviceAddress deviceAddress)
 {
-	sensors.requestTemperatures();
-	temp1C = sensors.getTempC(deviceAddress);
-	temp1F = sensors.getTempF(deviceAddress);
+  sensors.requestTemperatures();
+  temp1C = sensors.getTempC(deviceAddress);
+  temp1F = sensors.getTempF(deviceAddress);
 }
 
 //////////////////////////////////////////////////////////////////////////////////
 // displayData()
 //////////////////////////////////////////////////////////////////////////////////
-void displayData(){
-// Lcd5110 will display 14 characters x 6 rows
-	display.clearDisplay();   	// clears the screen and buffer
-	display.setTextSize(1);
-	display.setCursor(0,0);
-	display.setTextColor(BLACK);
-	
-	for (i=1; i <= NumNodes; i++){
-		display.print(i);
-		display.print(" ");
-		if(centigrade){
-			display.print(NodeTempC[i], 1);
-			display.print("C ");
-		}else{
-			display.print(NodeTempF[i], 1);
-			display.print("F ");
-		}
-		display.print(NodeVcc[i],1);
-		display.println("V");
-	}
+void displayData() {
+  // Lcd5110 will display 14 characters x 6 rows
+  display.clearDisplay();   	// clears the screen and buffer
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.setTextColor(BLACK);
 
-	display.print("Local: ");
-	if(centigrade){
-		display.print(temp1C, 1);
-		display.print("C ");
-	}else{
-		display.print(temp1F, 1);
-		display.print("F ");
-	}
-	display.display();
+  for (i = 1; i <= NumNodes; i++) {
+    display.print(i);
+    display.print(" ");
+    if (centigrade) {
+      display.print(NodeTempC[i], 1);
+      display.print("C ");
+    } else {
+      display.print(NodeTempF[i], 1);
+      display.print("F ");
+    }
+    display.print(NodeVcc[i], 1);
+    display.println("V");
+  }
 
+  display.print("Local: ");
+  if (centigrade) {
+    display.print(temp1C, 1);
+    display.print("C ");
+  } else {
+    display.print(temp1F, 1);
+    display.print("F ");
+  }
+  display.display();
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Serial print the temperatures in F
+//  temporaryly just print Outside,Attic,Office every time any data is received
+//  later need to modify so a node data is plotted only when that node transmits
+///////////////////////////////////////////////////////////////////////////
+void serialOut() {
+  Serial.print(NodeTempF[1], 1);
+  Serial.print(" ");
+  Serial.print(NodeTempF[2], 1);
+  Serial.print(" ");
+  Serial.println(temp1F, 1);
 }
 
 //////////////////////////////////////////////////////////////////////////////////
 // getRadioData()					// get Network data
 //////////////////////////////////////////////////////////////////////////////////
-void getRadioData(){
-	RF24NetworkHeader header;
-	payload_t payload;
-	bool done = false;
-      
-	while (!done){
+void getRadioData() {
+  RF24NetworkHeader header;
+  payload_t payload;
+  bool done = false;
 
-		done = 	network.read(header,&payload,sizeof(payload));
+  while (!done) {
 
-		NodeCounter[header.from_node] = payload.counter;
-		NodeTempC[header.from_node] = payload.tempC;
-		NodeTempF[header.from_node] = payload.tempF;
-		NodeVcc[header.from_node] = payload.Vcc;
-		
-		Serial.print("Received packet #");
-		Serial.print(NodeCounter[header.from_node]);
- 
-		Serial.print(" from node#: ");
-		Serial.print(header.from_node);
-		
-		Serial.print(" TempC: ");
-		Serial.print(NodeTempC[header.from_node], 1);
-		Serial.print(" TempF: ");
-		Serial.print(NodeTempF[header.from_node], 1);
-		Serial.print(" Vcc: ");
-		Serial.println(NodeVcc[header.from_node], 1);
-		Serial.print(" Local: Temp C:");
-                Serial.print(temp1C, 1);
-                Serial.print(" Temp F:");
-		Serial.println(temp1F, 1);
-	}
+    done = 	network.read(header, &payload, sizeof(payload));
+
+    NodeCounter[header.from_node] = payload.counter;
+    NodeTempC[header.from_node] = payload.tempC;
+    NodeTempF[header.from_node] = payload.tempF;
+    NodeVcc[header.from_node] = payload.Vcc;
+      serialOut();
+    /*
+    		Serial.print("Received packet #");
+    		Serial.print(NodeCounter[header.from_node]);
+
+    		Serial.print(" from node#: ");
+    		Serial.print(header.from_node);
+
+    		Serial.print(" TempC: ");
+    		Serial.print(NodeTempC[header.from_node], 1);
+    		Serial.print(" TempF: ");
+    		Serial.print(NodeTempF[header.from_node], 1);
+    		Serial.print(" Vcc: ");
+    		Serial.println(NodeVcc[header.from_node], 1);
+    		Serial.print(" Local: Temp C:");
+                    Serial.print(temp1C, 1);
+                    Serial.print(" Temp F:");
+    		Serial.println(temp1F, 1);
+    */
+  }
 }
 
 //////////////////////////////////////////////////////////////////////////////////
 // clearTemperatures()					// clear temps to make sure modules are active
 //////////////////////////////////////////////////////////////////////////////////
 
-void clearTemperatures(){
-	for (i=1; i <= NumNodes; i++){
-		NodeTempC[i] = 0.0;
-		NodeTempF[i] = 0.0;
-		NodeVcc[i] = 0.0;
-	}
+void clearTemperatures() {
+  for (i = 1; i <= NumNodes; i++) {
+    NodeTempC[i] = 0.0;
+    NodeTempF[i] = 0.0;
+    NodeVcc[i] = 0.0;
+  }
 }
